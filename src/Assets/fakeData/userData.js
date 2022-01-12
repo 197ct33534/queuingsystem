0fc7206bae21617f73ea65bb448ce6a19621f4b6
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-01-13 03:05:33 +0700

    doing dashboard

---
+++ src/Assets/fakeData/userData.js
@@ -0,0 +1,11 @@
+export const users = [
+    {
+        id: "1",
+        name: "nghia",
+        password: "123456",
+        fullname: "Lê Quỳnh Ái Vân",
+        phone: "123456789",
+        email: "adminSS01@domain.com",
+        job: "kế toán",
+    },
+];
